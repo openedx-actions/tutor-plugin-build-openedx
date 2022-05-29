4a1f19f626a2a51da098799b252160f3a554b7c2
lpm0073 <lpm0073@gmail.com>
2022-05-28 19:25:03 -0500

    dump info to console

--- scripts/add-requirements.sh
+++ scripts/add-requirements.sh
@@ -9,5 +9,11 @@ echo "adding requirementws"
 pwd
 ls -lha
 echo "parent looks like this..."
-ls -lha
+ls ../ -lha
+echo "tutor root is "
+echo "$(tutor config printroot)"
+echo "tutor directory contents:"
+ls $(tutor config printroot) -lha
+echo "PLUGINS_PATH: ${PLUGINS_PATH}"
+
 cp ./requirements/private.txt ${PLUGINS_PATH}
