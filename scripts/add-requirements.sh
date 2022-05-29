3ed66594f15e22496b66561d3d405dc500db37ab
lpm0073 <lpm0073@gmail.com>
2022-05-28 19:41:19 -0500

    re-add openedx custom code and cleanup console

--- scripts/add-requirements.sh
+++ scripts/add-requirements.sh
@@ -5,15 +5,4 @@
 #
 #---------------------------------------------------------
 
-echo "adding requirementws"
-pwd
-ls -lha
-echo "parent looks like this..."
-ls ../ -lha
-echo "tutor root is "
-echo "$(tutor config printroot)"
-echo "tutor directory contents:"
-ls $(tutor config printroot) -lha
-echo "PLUGINS_PATH: ${PLUGINS_PATH}"
-
 cp ./requirements/private.txt ${PLUGINS_PATH}
--- scripts/install-tutor.sh
+++ scripts/install-tutor.sh
@@ -5,7 +5,7 @@
 #
 #---------------------------------------------------------
 pip install --upgrade pyyaml
-echo "TUTOR_ROOT=./tutor" >> $GITHUB_ENV
+echo "TUTOR_ROOT=$GITHUB_ACTION_PATH/tutor" >> $GITHUB_ENV
 pip install tutor
 echo "TUTOR_VERSION=$(tutor --version | cut -f3 -d' ')" >> $GITHUB_ENV
 echo "TUTOR_ENABLE_HTTPS=true" >> $GITHUB_ENV
